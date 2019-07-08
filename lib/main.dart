49bbaa4711ef000d40e6e9e49c13ac8669c773ac
burrhanloli <burhanloli@gmail.com>
2019-07-08 22:58:09 +0530

    test-1

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,6 @@ class _AppState extends State<App> {
               return LoginScreen(userRepository: _userRepository);
             }
             if (state is Authenticated) {
-              //Added for test
               return NewHomeScreen();
             }
           },
